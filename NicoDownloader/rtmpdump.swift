1149b840c44841d65c4a72514d02f3e8cf32fdb8
kPherox <admin@mail.kr-kp.com>
2017-06-14 22:48:22 +0900

    add librtmp

---
+++ NicoDownloader/rtmpdump.swift
@@ -0,0 +1,14 @@
+//
+//  rtmpdump.swift
+//  NicoDownloader
+//
+//  Created by kPherox on 2017/06/13.
+//  Copyright © 2017 kPherox. All rights reserved.
+//
+
+import Cocoa
+import CRtmp
+
+
+class rtmpdump {
+}
